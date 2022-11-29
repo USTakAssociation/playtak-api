f62216e8a3a0ec905ed465f52e3601515815c2c6
Braydon Harris <braydonharris@gmail.com>
2022-11-29 15:06:25 -0700

    fix: unit test boilerplate

--- src/models/games-history/games.controller.spec.ts
+++ src/models/games-history/games.controller.spec.ts
@@ -1,12 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GamesController } from './games.controller';
+import { GamesService } from './games.service';
 
 describe('GamesController', () => {
 	let controller: GamesController;
 
+	const mockService = {
+	};
+	
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
 			controllers: [GamesController],
+			providers: [{
+				provide: GamesService,
+				useValue: mockService
+			}]
 		}).compile();
 
 		controller = module.get<GamesController>(GamesController);
--- src/models/games-history/games.service.spec.ts
+++ src/models/games-history/games.service.spec.ts
@@ -1,12 +1,41 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Games } from './entities/games.entity';
 import { GamesService } from './games.service';
 
 describe('GamesService', () => {
 	let service: GamesService;
 
+	const mockRepo = {
+		findAndCount: jest
+			.fn()
+			.mockImplementation(() => [[{ sn: '1234' }], 1]),
+		findOne: jest.fn(),
+		findByIds: jest.fn(),
+		find: jest.fn(),
+		save: jest.fn(),
+		update: jest.fn(),
+		delete: jest.fn(),
+		createQueryBuilder: jest.fn(() => ({
+			from: () => jest.fn(),
+			whereInIds: () => jest.fn(),
+			groupBy: () => jest.fn(),
+			delete: () => jest.fn(),
+			execute: () => jest.fn(),
+		})),
+		manager: {
+			connection: {
+				transaction: jest.fn(),
+			},
+		},
+	};
+		
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
-			providers: [GamesService],
+			providers: [GamesService, {
+				provide: getRepositoryToken(Games),
+				useValue: mockRepo
+			}],
 		}).compile();
 
 		service = module.get<GamesService>(GamesService);
