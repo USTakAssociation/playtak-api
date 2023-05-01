8deb0d9ddad34e5c85bf509b6d2c18e71877b7aa
nitzel <jan.s.92@web.de>
2023-05-01 13:10:17 +0200

    feat[TypeOrm]: synchronize=true for local development

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,7 +16,8 @@ import * as path from 'path';
 			type: 'sqlite',
 			database: path.join(process.env.DB_PATH, 'games.db'),
 			entities: [path.join(__dirname, '/**/*.entity{.ts,.js}')],
-			synchronize: false,
+			// Only re-create the DB Schemas in dev mode. For production this must be disabled.
+			synchronize: process.env['NODE_ENV'] == 'local',
 		}),
 		EventsModule,
 		GamesModule,
