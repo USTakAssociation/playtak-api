a3f7962be1f27150d45cbf3dc1240940231247d1
Braydon Harris <braydonharris@gmail.com>
2024-06-21 11:50:34 -0600

    fix: set cron value from environment variable

--- src/models/ratings/tasks/rating.task.ts
+++ src/models/ratings/tasks/rating.task.ts
@@ -1,14 +1,30 @@
-import { Injectable, Logger } from '@nestjs/common';
-import { Cron } from '@nestjs/schedule';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { Cron, SchedulerRegistry } from '@nestjs/schedule';
 import { RatingService } from '../ratings.service';
+import { CronJob } from 'cron';
 
 
 @Injectable()
-export class RatingTask {
+export class RatingTask implements OnModuleInit {
 	private readonly logger = new Logger(RatingTask.name);
 
-	constructor(private _service: RatingService) {}
+	constructor(
+		private _service: RatingService,
+		private schedulerRegistry: SchedulerRegistry
+	) {}
 
+	onModuleInit() {
+		this.logger.log('Rating task initialized');
+		if (process.env.RATING_CRON_VALUE) {
+			const job = new CronJob(process.env.RATING_CRON_VALUE, () => {
+				// What you want to do here
+				this.handleTask();
+			});
+			
+			this.schedulerRegistry.addCronJob('RatingTask', job);
+			job.start();
+		}
+	}
 	// * * * * * *
 	// | | | | | |
 	// | | | | | day of week
@@ -17,8 +33,8 @@ export class RatingTask {
 	// | | hours
 	// | minutes
 	// seconds (optional)
-	@Cron('0 15 * * * *')
-	handleCron() {
+	//@Cron('0 15 * * * *')
+	handleTask() {
 		this.logger.debug('Running rating task');
 		this._service.generateRating();
 	}
