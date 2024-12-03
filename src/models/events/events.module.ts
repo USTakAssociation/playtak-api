1c1d6515f39e8c631b6c113dec293d563e62907d
Braydon Harris <braydonharris@gmail.com>
2024-12-02 18:34:55 -0700

    chore: update dependencies refactor: update code for updated deps feat: add .nvmrc file to set node version

--- src/models/events/events.module.ts
+++ src/models/events/events.module.ts
@@ -1,5 +1,6 @@
 /* istanbul ignore file */
-import { CacheInterceptor, CacheModule, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
+import { CacheModule, CacheInterceptor } from '@nestjs/cache-manager';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { ThrottlerModule } from '@nestjs/throttler';
 import { EventsController } from './events.controller';
@@ -9,10 +10,10 @@ import { EventsService } from './events.service';
 	controllers: [EventsController],
 	imports: [
 		ThrottlerModule.forRootAsync({
-			useFactory: () => ({
+			useFactory: () => [{
 				ttl: 60,
 				limit: 30,
-			}),
+			}],
 		}),
 		CacheModule.register({
 			ttl: 300, // seconds
